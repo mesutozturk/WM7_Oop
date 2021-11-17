d1ea1e53c2fb6df2f31cdad12471926662fdf4f0
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2021-11-17 17:35:37 +0300

    Add project files.

---
+++ KisiEnvanter.Lib/Kisi.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace KisiEnvanter.Lib
+{
+    public class Kisi
+    {
+
+        /*
+         * Access Modifier 
+         * Private
+         * Public
+         * Internal
+         * *Protected
+         * *Protected Internal
+         */
+        private string _ad;
+        private string _soyad;
+        private int _yas;
+
+        //Encapsulation
+        //Property
+        public string Ad
+        {
+            set
+            {
+                if (value.Length < 2)
+                    throw new Exception("Ad En az 2 karakter olmalı");
+                foreach (char harf in value)
+                {
+                    if (char.IsDigit(harf) || char.IsPunctuation(harf) || char.IsSymbol(harf))
+                        throw new Exception("Ad ifadesi içerisinde rakam kullanamazsın!");
+                }
+                _ad = value;
+            }
+            get
+            {
+                string son = string.Empty;
+                string[] isimler = _ad.Split(' ');
+                foreach (string isim in isimler)
+                {
+                    son += isim.Substring(0, 1).ToUpper() + isim.Substring(1) + " ";
+                }
+                return son.Remove(son.Length - 1);
+
+                //string.Join(' ', isimler);
+
+                //return son + "\b";
+            }
+        }
+
+    }
+}
---
+++ KisiEnvanter.WFA/Form1.cs
@@ -0,0 +1,25 @@
+﻿using KisiEnvanter.Lib;
+using System;
+using System.Windows.Forms;
+
+namespace KisiEnvanter.WFA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Kisi yeniKisi = new Kisi();
+            yeniKisi.Ad = "ahmet ++++nur çebi";
+            //yeniKisi.Soyad = "Fıdıl";
+            //yeniKisi.Yas = 30;
+
+            this.Text = yeniKisi.Ad;
+
+        }
+    }
+}
