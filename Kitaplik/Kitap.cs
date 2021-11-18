91e68b1f41a98868f1e252e5d4e4b42e6f9f4835
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2021-11-18 17:00:30 +0300

    gün 2

---
+++ Kitaplik/Kitap.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Kitaplik
+{
+    public class Kitap
+    {
+        public Kitap()
+        {
+            EklenmeZamani = DateTime.Now;
+        }
+        public string Ad { get; set; }
+        public int Yil { get; set; }
+        public int Baski { get; set; }
+        public string YazarAdSoyad { get; set; }
+        public DateTime EklenmeZamani { get; private set; }
+    }
+}
