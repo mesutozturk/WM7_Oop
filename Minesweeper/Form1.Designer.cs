8d311e04215dcaf3a9433e5a537d31c8ca05b838
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2021-11-18 20:56:43 +0300

    mayın tarlası

---
+++ Minesweeper/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿
+namespace Minesweeper
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.oyunToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.yeniOyunToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.çıkışToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.flp1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.oyunToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // oyunToolStripMenuItem
+            // 
+            this.oyunToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.yeniOyunToolStripMenuItem,
+            this.çıkışToolStripMenuItem});
+            this.oyunToolStripMenuItem.Name = "oyunToolStripMenuItem";
+            this.oyunToolStripMenuItem.Size = new System.Drawing.Size(57, 24);
+            this.oyunToolStripMenuItem.Text = "Oyun";
+            // 
+            // yeniOyunToolStripMenuItem
+            // 
+            this.yeniOyunToolStripMenuItem.Name = "yeniOyunToolStripMenuItem";
+            this.yeniOyunToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.yeniOyunToolStripMenuItem.Text = "Yeni Oyun";
+            this.yeniOyunToolStripMenuItem.Click += new System.EventHandler(this.yeniOyunToolStripMenuItem_Click);
+            // 
+            // çıkışToolStripMenuItem
+            // 
+            this.çıkışToolStripMenuItem.Name = "çıkışToolStripMenuItem";
+            this.çıkışToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.çıkışToolStripMenuItem.Text = "Çıkış";
+            // 
+            // flp1
+            // 
+            this.flp1.Location = new System.Drawing.Point(155, 62);
+            this.flp1.Name = "flp1";
+            this.flp1.Size = new System.Drawing.Size(569, 424);
+            this.flp1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 607);
+            this.Controls.Add(this.flp1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem oyunToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem yeniOyunToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem çıkışToolStripMenuItem;
+        private System.Windows.Forms.FlowLayoutPanel flp1;
+    }
+}
---
+++ Minesweeper/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Minesweeper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private const int Count = 16;
+        private List<bool> _mineStatus = new();
+        private Color _color = Color.Gray, _colorBomb = Color.Tomato, _colorFree = Color.LimeGreen;
+
+        private void yeniOyunToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            flp1.Controls.Clear();
+            _mineStatus = new();
+            Random rnd = new();
+            for (int i = 0; i < Count; i++)
+            {
+                bool durum = rnd.Next() % 2 == 0;
+                _mineStatus.Add(durum);
+
+                Button btn = new Button()
+                {
+                    Name = $"btn_{i + 1}",
+                    Text = "X",
+                    Size = new Size(135, 97),
+                    BackColor = _color,
+                    Tag = i
+                };
+                btn.Click += Btn_Click;
+                flp1.Controls.Add(btn);
+            }
+            //Linq 
+            this.Text = $"Bomba Sayısı: {_mineStatus.Count(x => x)}";
+        }
+
+        private void Btn_Click(object? sender, EventArgs e)
+        {
+            Button selectedButton = sender as Button;
+            int index = Convert.ToInt32(selectedButton.Tag);
+            if (_mineStatus[index])
+            {
+                selectedButton.BackColor = _colorBomb;
+                MessageBox.Show("Bitti");
+            }
+            else
+            {
+                selectedButton.BackColor = _colorFree;
+            }
+        }
+    }
+}
