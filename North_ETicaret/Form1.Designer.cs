1ad49c1c3c6b9346ae2f50e0b7267dac6b5bdde0
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2021-11-22 18:39:03 +0300

    dbfirst 1

---
+++ North_ETicaret/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace North_ETicaret
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstCategory = new System.Windows.Forms.ListBox();
+            this.lstProduct = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lstCategory
+            // 
+            this.lstCategory.FormattingEnabled = true;
+            this.lstCategory.ItemHeight = 20;
+            this.lstCategory.Location = new System.Drawing.Point(12, 12);
+            this.lstCategory.Name = "lstCategory";
+            this.lstCategory.Size = new System.Drawing.Size(310, 644);
+            this.lstCategory.TabIndex = 0;
+            // 
+            // lstProduct
+            // 
+            this.lstProduct.FormattingEnabled = true;
+            this.lstProduct.ItemHeight = 20;
+            this.lstProduct.Location = new System.Drawing.Point(328, 12);
+            this.lstProduct.Name = "lstProduct";
+            this.lstProduct.Size = new System.Drawing.Size(310, 644);
+            this.lstProduct.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(657, 673);
+            this.Controls.Add(this.lstProduct);
+            this.Controls.Add(this.lstCategory);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstCategory;
+        private System.Windows.Forms.ListBox lstProduct;
+    }
+}
---
+++ North_ETicaret/Form1.cs
@@ -0,0 +1,27 @@
+﻿using North_ETicaret.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace North_ETicaret
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        NorthwindContext _dbContext = new NorthwindContext();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lstCategory.DataSource = _dbContext.Categories.ToList();
+            lstCategory.DisplayMember = "CategoryName";
+        }
+    }
+}
